df13f8b37968b25e5d72b69fe61da4a4941929a1
krishna_siddamsetti <155523010+Krishna-siddamsetti73@users.noreply.github.com>
2026-04-28 19:10:37 +0530

    Update link in Navbar component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -498,7 +498,7 @@ export function Navbar() {
 
                 <div className="pt-3">
                   <a
-                    href="https://earth.google.com/earth/d/1MS-DYbZVeEuiRP8LbFLcjeS-nd0V7S2c?usp=sharing"
+                    href="https://earth.google.com/earth/d/1gvZGGSD4MdlONdowzB2MOC5xr-zprqJl?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
